99ab454e8759716aff6776cead088dde3673a71d
Demur Rumed <gunkmute@gmail.com>
2016-01-20 04:34:15 +0000

    Implement a room generator

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
+extern crate rand;
 extern crate ncurses;
 use std::char;
 use std::cell::RefCell;
 use std::collections::HashSet;
+use rand::*;
+use rand::distributions::{IndependentSample, Range};
 use ncurses::*;
 
 #[derive(Clone, Copy)]
 enum Dir { E, NE, N, NW, W, SW, S, SE }
-enum Action {
-	Move(Dir),
-	Aim(Dir),
-}
 fn ch2dir(c: char) -> Option<Dir> {
 	Some(match c {
 		'l' => Dir::E,
@@ -40,6 +39,14 @@ fn isdiag(d: Dir) -> bool {
 		Dir::E | Dir::N | Dir::W | Dir::S => false,
 		Dir::NE | Dir::NW | Dir::SW | Dir::SE => true,
 	}
+}
+fn calcdist2(xy1: (i32, i32), xy2: (i32, i32)) -> i32{
+	let (x1, y1) = xy1;
+	let (x2, y2) = xy2;
+	(x1-x2)*(y1-y2) // TODO calc with diag limitation
+}
+fn rectover(r1: &(i32,i32,i32,i32), r2: &(i32,i32,i32,i32)) -> bool {
+	r1.0 <= r2.2 && r1.2 >= r2.0 && r1.1 <= r2.3 && r1.3 >= r2.1
 }
 //fn dir2rad(d: Dir) -> f64 {
 //	(dir2i32(d) as f64)*3.141592
@@ -93,13 +100,18 @@ impl Obj for Player{
 struct Wall{
 	xy: (i32, i32)
 }
+impl Wall{
+	fn new(xy: (i32, i32)) -> Self {
+		Wall { xy: xy }
+	}
+}
 impl Obj for Wall{
 	fn xy(&self) -> (i32, i32){ self.xy }
 	fn mv(&mut self, xy: (i32, i32)) -> (i32, i32) { self.xy = xy; xy }
 	fn ch(&self) -> char { '#' }
 }
 struct Room<'a>{
-	p: &'a mut Player,
+	p: Player,
 	o: Vec<RefCell<Box<Obj + 'a>>>,
 	w: i32,
 	h: i32,
@@ -112,11 +124,90 @@ impl<'a> Room<'a> {
 		if xy == self.p.xy() { self.p.ch() }
 		else { self.o.iter().find(|&o| o.borrow().xy() == xy).map(|o| o.borrow().ch()).unwrap_or(' ') }
 	}
-	pub fn tock(&mut self) -> () {
+	pub fn tock(&mut self) {
 		self.t += 1;
 		self.p.tock();
 		for o in self.o.iter() {
 			o.borrow_mut().tock();
+		}
+	}
+}
+
+trait RoomGenerator{
+	fn generate(&self, i32, i32) -> Room;
+}
+struct RogueRoomGen{
+	pxy: (i32, i32),
+	rc: u32,
+}
+impl Default for RogueRoomGen {
+	fn default() -> Self {
+		RogueRoomGen {
+			pxy: (3, 3),
+			rc: 6,
+		}
+	}
+}
+impl RoomGenerator for RogueRoomGen {
+	fn generate(&self, w: i32, h: i32) -> Room {
+		let betw = Range::new(0, w-2);
+		let beth = Range::new(0, h-2);
+		let bet4 = Range::new(0, 4);
+		let mut rng = rand::thread_rng();
+		let mut rxy: Vec<(i32, i32, i32, i32)> = Vec::new();
+		while rxy.len() < (self.rc as usize) {
+			let rx = betw.ind_sample(&mut rng);
+			let ry = beth.ind_sample(&mut rng);
+			let candy = (rx, ry, rx+2, ry+2);
+			println!("\r{:?}\t{:?}", rxy, candy);
+			if !rxy.iter().any(|a| rectover(&candy, a))
+				{ rxy.push(candy) }
+		}
+		loop {
+			let mut cangrow: Vec<bool> = Vec::new();
+			let b4 = bet4.ind_sample(&mut rng);
+			for rect in &rxy {
+				let newrect = match b4 {
+					0 => (rect.0-1, rect.1, rect.2, rect.3),
+					1 => (rect.0, rect.1-1, rect.2, rect.3),
+					2 => (rect.0, rect.1, rect.2+1, rect.3),
+					3 => (rect.0, rect.1, rect.2, rect.3+1),
+					_ => unreachable!(),
+				};
+				cangrow.push(newrect.0 >= 0 && newrect.1 >= 0 && newrect.2 < w && newrect.3 < h && rxy.iter().filter(|rect2| rectover(&newrect, rect2) ).take(2).count() == 1);
+			}
+			println!("\r{:?}\t{:?}", cangrow, rxy);
+			if cangrow.iter().all(|&x| !x) { break }
+			for (&mut (ref mut x1, ref mut y1, ref mut x2, ref mut y2), &grow) in rxy.iter_mut().zip(&cangrow) {
+				println!("\r{},{},{},{},{}",x1,y1,x2,y2,grow);
+				if !grow { continue }
+				match b4 {
+					0 => *x1 -= 1,
+					1 => *y1 -= 1,
+					2 => *x2 += 1,
+					3 => *y2 += 1,
+					_ => unreachable!(),
+				}
+				println!("\r{},{},{},{},{}",x1,y1,x2,y2,grow);
+			}
+		}
+		let mut ovec = Vec::<RefCell<Box<Obj>>>::new();
+		for xywh in rxy {
+			for x in xywh.0..xywh.2+1 {
+				ovec.push(RefCell::new(Box::new(Wall { xy: (x,xywh.1) })));
+				ovec.push(RefCell::new(Box::new(Wall { xy: (x,xywh.3) })));
+			}
+			for y in xywh.1..xywh.3+1 {
+				ovec.push(RefCell::new(Box::new(Wall { xy: (xywh.0,y) })));
+				ovec.push(RefCell::new(Box::new(Wall { xy: (xywh.2,y) })));
+			}
+		}
+		Room {
+			p: Player { xy: self.pxy, ticks: 0 },
+			o: ovec,
+			w: w,
+			h: h,
+			t: 0,
 		}
 	}
 }
@@ -151,16 +242,8 @@ fn main(){
 	initscr();
 	raw();
 	noecho();
-	let mut p = Player { xy: (2, 2), ticks: 0 };
-	let mut room = Room {
-		p: &mut p,
-		o: vec![
-			RefCell::new(Box::new(Wall {xy: (3,3)})),
-			RefCell::new(Box::new(Wall {xy: (3,4444)}))],
-		w: 40,
-		h: 40,
-		t: 0,
-	};
+	let rrg = RogueRoomGen::default();
+	let mut room = rrg.generate(40, 40);
 	loop{
 		room.tock();
 		if room.p.ticks == 0 {
