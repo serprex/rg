20fc19225c4875e520e8b8b19345c1efa2e79791
Demur Rumed <gunkmute@gmail.com>
2016-10-17 22:55:59 +0000

    Floodfill to make sure forest doesn't become impossibly tangled

--- src/genroom_forest.rs
+++ src/genroom_forest.rs
@@ -1,3 +1,4 @@
+use fnv::FnvHashSet;
 use rand::Rng;
 use rand::distributions::{Range, IndependentSample};
 use specs::World;
@@ -18,9 +19,9 @@ pub struct ForestRoomGen {
 impl Default for ForestRoomGen {
 	fn default() -> Self {
 		ForestRoomGen {
-			trees: 3,
+			trees: 4,
 			raff: 1,
-			null: 6,
+			null: 9,
 		}
 	}
 }
@@ -34,8 +35,11 @@ impl RoomGen for ForestRoomGen {
 			.with(Weight(2))
 			.with(Atk::<Weapon>::new(1, 2, 2))
 			.build();
+		let exithash: FnvHashSet<[i16; 2]> = exits.iter().cloned().collect();
+
 		for x in xyz[0]..xyz[0]+w {
 			for y in xyz[1]..xyz[1]+h {
+				if exithash.contains(&[x, y]) { continue }
 				let r = range.ind_sample(rng);
 				if r < self.trees {
 					let Walls(ref mut walls) = *room.write_resource::<Walls>();
@@ -57,6 +61,73 @@ impl RoomGen for ForestRoomGen {
 					let mut possy = room.write_resource::<Possy>();
 					possy.set_pos(e, [x, y, xyz[2]]);
 				}
+			}
+		}
+
+		let mut xys: FnvHashSet<[i16; 2]> = Default::default();
+		let mut ffs = Vec::new();
+		let mut candy = Vec::new();
+		let Walls(ref mut walls) = *room.write_resource::<Walls>();
+		'xyloop:
+		for x in xyz[0]..xyz[0]+w {
+			for y in xyz[1]..xyz[1]+h {
+				if !walls.contains_key(&[x, y, xyz[2]]) {
+					ffs.push([x, y]);
+					break 'xyloop
+				}
+			}
+		}
+		loop {
+			candy.clear();
+			while let Some(xy) = ffs.pop() {
+				xys.insert(xy);
+				for &(x, y, b) in &[(xy[0]+1, xy[1], xy[0]+1 < xyz[0] + w),
+					(xy[0], xy[1]+1, xy[1]+1 < xyz[1] + h),
+					(xy[0]-1, xy[1], xy[0]-1 >= xyz[0]),
+					(xy[0], xy[1]-1, xy[1]-1 >= xyz[1])
+				] {
+					if b && !walls.contains_key(&[x, y, xyz[2]]) && !xys.contains(&[x, y]) {
+						ffs.push([x, y]);
+					}
+				}
+			}
+			// TODO we won't detect a 2-tile thick wall divide. FIX scan for first non-wall tile
+			for x in xyz[0]..xyz[0]+w {
+				for y in xyz[1]..xyz[1]+h {
+					if walls.contains_key(&[x, y, xyz[2]]) {
+						for &(xd, yd, x1d, y1d, x2d, y2d, x3d, y3d) in &[
+							(0, 1, 0, -1, 1, 0, -1, 0),
+							(1, 0, 0, -1, 0, 1, -1, 0),
+							(-1, 0, 0, 1, 1, 0, 0, -1),
+							(0, -1, 0, 1, 1, 0, -1, 0),
+						] {
+							if !walls.contains_key(&[x+xd, y+yd, xyz[2]]) && !xys.contains(&[x+xd, y+yd]) && (
+								xys.contains(&[x+x1d, y+y1d]) ||
+								xys.contains(&[x+x2d, y+y2d]) ||
+								xys.contains(&[x+x3d, y+y3d]))
+							{
+								candy.push((x, y));
+								break
+							}
+						}
+					}
+				}
+			}
+			if let Some(&(x, y)) = rng.choose(&candy) {
+				walls.remove(&[x, y, xyz[2]]);
+				for &(xd, yd, b) in &[
+					(1, 0, x + 1 < xyz[0] + w),
+					(0, 1, y + 1 < xyz[1] + h),
+					(-1, 0, x - 1 >= xyz[0]),
+					(0, -1, y - 1 >= xyz[1]),
+				] {
+					if b && !xys.contains(&[x+xd, y+yd]) && !walls.contains_key(&[x+xd, y+yd, xyz[2]])
+					{
+						ffs.push([x+xd, y+yd]);
+					}
+				}
+			} else {
+				break
 			}
 		}
 	}
